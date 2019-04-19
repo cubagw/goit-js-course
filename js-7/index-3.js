2d0ebd51c90c37d60b3bde461d02fb653c78be8f
dmytro vitenko <cubagw84@gmail.com>
2019-04-20 00:21:57 +0300

    4.1

--- js-7/index-3.js
+++ js-7/index-3.js
@@ -54,10 +54,10 @@ const addLogin = function(allLogins, login) {
 };
 
 // Вызовы функции для проверки
-console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
 console.log(addLogin(logins, 'Ajax')); //Контрольная проверка
 console.log('Обновленный масив - ', logins); //Обновленый масив
