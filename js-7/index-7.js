e5dad8c0ac32532d4520868d8619c01be83b087c
dmytro vitenko <cubagw84@gmail.com>
2019-05-15 10:49:39 +0300

    7.2

--- js-7/index-7.js
+++ js-7/index-7.js
@@ -116,7 +116,7 @@ console.log(getUsersByEyeColor(users, 'blue')); // [объект Moore Hensley, 
 // Получить массив имен пользователей по полу (поле gender).
 
 const getUsersByGender = (users, gender) =>
-  users.filter(user => user.gender === gender);
+  users.filter(user => user.gender === gender).map(user => user.name);
 
 console.log(getUsersByGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
@@ -166,7 +166,7 @@ console.log(getTotalBalance(users)); // 20916
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersByFriend = (users, name) =>
-  users.filter(user => user.friends.includes(name));
+  users.filter(user => user.friends.includes(name)).map(user => user.name);
 
 console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
